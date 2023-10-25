60a24847e99bed44ff73d903b25baa2d43b71c36
hernan villalba <hernan.p.villalba@outlook.com>
2023-10-25 19:19:27 -0300

    segunda preentrega

--- main.js
+++ main.js
@@ -5,42 +5,63 @@ function pedirDatos(){
     alert (`${saludoCompleto}, bienvenido a Detailing Vehicular`);
 }
 pedirDatos()
-// while(nombre == Number ){
-//     alert("Por favor, ingrese su nombre");
-//     nombre = prompt ("Por favor, ingrese su nombre");
-// }
 
 
-function ingreseCelular(){
-    let numeroTelefono = (prompt("Por favor, ingrese su numero de celular sin el 15, por ej. 65923031"));
-    if  (numeroTelefono > 99999999) {
-        alert ("Numero no valido");
-    }
-    else{
-        alert("Selecciona el turno deseado");
-    }    
-    }
-
-ingreseCelular()
-
-function elegirTurno(){
-    let horarioTurno = (prompt("Por favor indicanos el turno deseado: Turno 1. 08:00 Turno 2. 10:00 Turno 3. 12:00 Turno 4. 14:00"))
-
-    switch (horarioTurno) {
-        case "1":
-            alert ("Turno 1: 8hs ")
-            break;
-        case "2":
-            alert ("Turno 2: 10hs ")
-            break;
-        case "3":
-            alert ("Turno 3: 12hs ")
-            break;
-        case "4":
-            alert ("Turno 4: 14hs ")
-            break;
-        default:
-            alert ("Opcion no valida")
+class Producto {
+    constructor (nombre, marca, precio, cantidad){
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+        this.cantidad = cantidad;
     }
 }
-elegirTurno()
+
+const productos = [];
+productos.push(new Producto ("shampoo", "toxic shane shampoo", 2150, 10));
+productos.push(new Producto ("cera","cera líquida toxic shane", 2700, 10));
+productos.push(new Producto ("paño","paño microfibra Laffitte", 4200, 10));
+productos.push(new Producto ("esponja","esponja de microfibra", 2800, 15));
+productos.push(new Producto ("cepillo","cepillo para llantas", 6500, 5));
+
+let nombre = prompt ("Ingresa el producto que buscas: shampoo / cera / paño / esponja / cepillo").toLowerCase();
+while (nombre != "shampoo" && nombre != "cera" && nombre != "paño" && nombre != "esponja" && nombre != "cepillo" ){
+    alert ("No es un producto valido")
+    nombre = prompt ("Ingresa el producto que buscas: shampoo / cera / paño / esponja / cepillo").toLowerCase();
+}
+
+const buscador = productos.find((item) => item.nombre === nombre);
+
+if (buscador){
+    alert(`
+    Marca: ${buscador.marca}
+    Precio: ${buscador.precio}
+    Cantidad Disponible: ${buscador.cantidad} 
+    `);
+} 
+
+
+let compra = prompt ("Queres comprarlo? si/no").toLowerCase()
+if (compra == "no"){
+    alert("Gracias, vuelva prontos");
+} else if (compra == "si"){
+    switch(productos){
+        case "shampoo":
+            break;
+        case "cera":
+            break;
+        case "paño":
+            break;
+        case "esponja":
+            break;
+        case "cepillo":
+            break;
+        default:
+            break;
+    }
+    let unidades = parseInt (prompt("Ingresa la cantidad deseada"));
+    if( unidades > buscador.cantidad ) {
+        alert ("No hay stock disponible");
+    } else{
+        alert ("Gracias por tu compra")
+    }
+}
