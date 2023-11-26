bfbd160397072c82fd19016ae193fe56b9b38805
hernan villalba <hernan.p.villalba@outlook.com>
2023-11-26 18:30:30 -0300

    entrega final

--- main.js
+++ main.js
@@ -1,91 +1,136 @@
-class Producto {
-    constructor (nombre, marca, precio, cantidad){
-        this.nombre = nombre;
-        this.marca = marca;
-        this.precio = precio;
-        this.cantidad = cantidad;
-    }
-}
+const productosVenta = document.getElementById("productosVenta");
+const carritoCompra = document.getElementById("carritoCompra");
+const compras = document.getElementById("compras");
 
-const productos = [];
-productos.push(new Producto ("shampoo", "Toxic Shane Shampoo", 2150, 10));
-productos.push(new Producto ("cera","Cera Líquida Toxic Shane", 2700, 10));
-productos.push(new Producto ("paño","Paño Microfibra Laffitte", 4200, 10));
-productos.push(new Producto ("esponja","Esponja De Microfibra", 2800, 15));
-productos.push(new Producto ("cepillo","Cepillo Para Llantas", 6500, 5));
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-const buscador = document.getElementById("buscador");
-const listaResultado = document.getElementById("listaResultado");
-const sinResultado = document.getElementById ("sinResultado");
+const getProductos = async () =>{
+    const response = await fetch("productos.json");
+    const data = await response.json();
 
-const verCarrito = document.getElementById("verCarrito");
-const contenidoCarrito = document.getElementById("contenidoCarrito");
-
-let carrito = [];
-
-const buscadorProducto = () => {
-    const buscarProducto = buscador.value.toLowerCase();
-    const filtradoProducto = productos.filter((item) => item.nombre.toLowerCase().startsWith(buscarProducto));
-
-    listaResultado.innerHTML = "";
-
-    if(filtradoProducto.length === 0){
-        sinResultado.style.display = "block";
-    } else{
-        filtradoProducto.forEach((productos) => {
-            let div = document.createElement("div");
-            div.innerHTML = `
-            <h2>Marca: ${productos.marca}</h2>
-            <p>Nombre: ${productos.nombre}</p>
-            <b>Precio: ${productos.precio} </b>
-            `;
-            listaResultado.append(div);
-
-            let comprar = document.createElement("button");
-            comprar.innerText = "Comprar"; 
-            div.append(comprar);
-
-            comprar.addEventListener("click",() =>{
-                carrito.push({
-                    nombre: productos.nombre,
-                    marca: productos.marca,
-                    precio: productos.precio,
+    data.forEach ((item) => {
+        let content = document.createElement("div");
+        content.className = "shop";
+        content.innerHTML = `
+            <img src="${item.imagen}">
+            <h3> ${item.nombre} </h3>
+            <p>$ ${item.precio} </p>
+        `;
+        productosVenta.append(content);
+    
+        let comprar = document.createElement ("button");
+        comprar.innerText = "Comprar";
+        comprar.className = "boton-comprar"
+    
+        content.append(comprar);
+    
+        comprar.addEventListener("click",() =>{
+            Swal.fire({
+                title: "Agregado al Carrito",
+                text: "Clickea en el auto para ver el total",
+                icon: "success"
+              });
+            const repeat = carrito.some((repetirProducto) => repetirProducto.nombre === item.nombre);
+            if (repeat === true){
+                carrito.map((articulos) =>{
+                    if(articulos.nombre === item.nombre){
+                        articulos.cantidad++;
+                    }
                 });
-                console.log(carrito);
+            }else{
+                carrito.push({
+                nombre: item.nombre,
+                precio: item.precio,
+                imagen: item.imagen,
+                cantidad: item.cantidad,
+                });
+                guardadoLocal();
+            }
+        });
+    });
+    
+    const verCarrito = () =>{
+        compras.innerHTML = "";
+        compras.style.display = "flex";
+        const carritoHeader = document.createElement ("div");
+        carritoHeader.className = "carrito-header"
+        carritoHeader.innerHTML = `
+            <h2 class = "carrito-header-titulo"> Carrito </h2>
+        `;
+    
+        compras.append(carritoHeader);
+    
+        const carritoBoton = document.createElement ("h2");
+        carritoBoton.innerText = "X";
+        carritoBoton.className = "carrito-boton-header";
+    
+        carritoBoton.addEventListener("click",() =>{
+            compras.style.display = "none";
+        });
+    
+        carritoHeader.append(carritoBoton);
+    
+        carrito.forEach((item) =>{
+            let carritoContent = document.createElement ("div");
+            carritoContent.className = "carrito-content"
+            carritoContent.innerHTML = `
+                <img src="${item.imagen}">
+                <h3> ${item.nombre} </h3>
+                <p>Precio:$${item.precio} </p>
+                <p>Cantidad:${item.cantidad} </p>
+                <p>Total:$${item.cantidad * item.precio} </p>
+                `;
+            compras.append(carritoContent);
+    
+            let eliminar  = document.createElement ("button");
+            eliminar.innerText = "Eliminar";
+            eliminar.className = "eliminar-producto";
+            carritoContent.append(eliminar);
+    
+            eliminar.addEventListener("click", () =>{
+                Swal.fire({
+                    title: "Queres eliminar el producto?",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "Si",
+                    cancelButtonText: "No",
+                }).then ((result) =>{
+                    if (result.isConfirmed){
+                        Swal.fire({
+                            title: "Eliminado",
+                            icon: "success",
+                            text: "Producto eliminado del carrito",
+                        })
+                        eliminarProducto();
+                    }
+                })
             });
         });
-        sinResultado.style.display = "none";
-    }
-    if(buscador.value === "") {
-        listaResultado.innerHTML = "";
-    }
+
+        const total = carrito.reduce((acc, pr) => acc + pr.precio * pr.cantidad, 0);
+    
+        const totalCompra = document.createElement ("div");
+        totalCompra.className = "total-compra";
+        totalCompra.innerHTML = `Total a Pagar:$${total}`;
+        
+        compras.append(totalCompra);
+    };
+    
+    carritoCompra.addEventListener("click", verCarrito);
+    
+    const eliminarProducto = () =>{
+        const foundNombre = carrito.find ((articulo) => articulo.nombre);
+        carrito = carrito.filter((carritoNombre) =>{
+            return carritoNombre !== foundNombre;
+        });
+    
+        verCarrito();
+        guardadoLocal();
+    };
 };
 
-buscador.addEventListener("input", buscadorProducto);
+getProductos();
 
-verCarrito.addEventListener ("click", () =>{
-    const carritoCompra = document.createElement("div");
-    carritoCompra.innerHTML=`
-    <h1> Carrito de compras </h1>
-    `;
-    contenidoCarrito.append(carritoCompra);
-
-    carrito.forEach((productos) =>{
-        let carritoContent = document.createElement ("div");
-        carritoContent.innerHTML = `
-            <h2>Marca: ${productos.marca}</h2>
-            <p>Nombre: ${productos.nombre}</p>
-            <b>Precio: ${productos.precio} </b>
-    `;
-    contenidoCarrito.append(carritoContent);
-    }); 
-
-    const total = carrito.reduce((acc, el) => acc + el.precio,0);
-
-    const totalCompra = document.createElement ("div");
-    totalCompra.innerHTML = `<h3>total a pagar: ${total} $</h3>`;
-    contenidoCarrito.append(totalCompra);
-});
-
-localStorage.setItem ("articulos", JSON.stringify(productos));
-let articulos = JSON.parse (localStorage.getItem("productos"));
+const guardadoLocal = () =>{
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
